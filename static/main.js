5a7bba87e281fdc6a76bb1bf4977b6654ea65475
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-05-25 18:12:50 -0400

    run prettier

--- static/main.js
+++ static/main.js
@@ -5,22 +5,29 @@ function getStarted() {
 window.onload = function () {
   let startlistener = document.getElementById("startbutton");
   startlistener.addEventListener("click", getStarted);
-}
+};
 
-AFRAME.registerComponent('markerhandler', {
+AFRAME.registerComponent("markerhandler", {
   init: function () {
-    this.markers = { reference: false }
-    document.querySelector("#anchorzero").addEventListener('targetFound', () => {
-      this.markers.reference = true;
-      document.querySelector('#goalzero').innerHTML = "Reference Desk ✔";
-      this.check(this.markers);
-    });
+    this.markers = { reference: false };
+    document
+      .querySelector("#anchorzero")
+      .addEventListener("targetFound", () => {
+        this.markers.reference = true;
+        document.querySelector("#goalzero").innerHTML = "Reference Desk ✔";
+        this.check(this.markers);
+      });
   },
   check: function (markers) {
-     if (Object.is(JSON.stringify(markers), JSON.stringify({ reference: true })) == true) {
-       document.getElementById("starter").style.display = "none";
-       document.getElementById("ender").style.display = "block";
-       document.getElementsByClassName("dropdown-menu")[0].classList.remove("show");
+    if (
+      Object.is(JSON.stringify(markers), JSON.stringify({ reference: true })) ==
+      true
+    ) {
+      document.getElementById("starter").style.display = "none";
+      document.getElementById("ender").style.display = "block";
+      document
+        .getElementsByClassName("dropdown-menu")[0]
+        .classList.remove("show");
     }
   },
 });
