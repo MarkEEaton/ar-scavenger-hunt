b95eceb9f37690cc41c0a2a19d84269c2b64ff58
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-05-31 12:27:44 -0400

    add response headers

--- app.py
+++ app.py
@@ -8,5 +8,16 @@ def index():
     return render_template("index.html")
 
 
+@app.after_request
+def add_headers(resp):
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    resp.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    resp.headers["Content-Security-Policy"] = (
+        "script-src 'self'; style-src 'self'; default-src 'none'"
+    )
+    resp.headers["Cache-Control"] = "max-age=0"
+    return resp
+
+
 if __name__ == "__main__":
     app.run(debug=True)
