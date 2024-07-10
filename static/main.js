e76714d57e6d566eeb366b2ed2606b249f6fca6a
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-07-10 10:55:17 -0400

    fully working with photo markers

--- static/main.js
+++ static/main.js
@@ -9,46 +9,46 @@ window.onload = function () {
 
 AFRAME.registerComponent("markerhandler", {
   init: function () {
-    this.markers = { reference: false };
+    this.markers = { reference: false, computers: false, referencebooks: false, studyrooms: false, leisure: false, photocopier: false, elevator: false, circulation: false }
     document
       .querySelector("#anchorzero")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.circulation = true;
         document.querySelector("#goalzero").innerHTML = "Circulation Desk ✔";
         this.check(this.markers);
       });
     document
       .querySelector("#anchorone")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.elevator = true;
         document.querySelector("#goalone").innerHTML = "Elevators ✔";
         this.check(this.markers);
       });
     document
       .querySelector("#anchortwo")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.leisure = true;
         document.querySelector("#goaltwo").innerHTML = "Leisure Collection ✔";
         this.check(this.markers);
       });
     document
       .querySelector("#anchorthree")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.photocopier = true;
         document.querySelector("#goalthree").innerHTML = "Photocopier ✔";
         this.check(this.markers);
       });
     document
       .querySelector("#anchorfour")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.computers = true;
         document.querySelector("#goalfour").innerHTML = "Printer ✔";
         this.check(this.markers);
       });
     document
       .querySelector("#anchorfive")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.referencebooks = true;
         document.querySelector("#goalfive").innerHTML = "Reference Collection ✔";
         this.check(this.markers);
       });
@@ -62,15 +62,14 @@ AFRAME.registerComponent("markerhandler", {
     document
       .querySelector("#anchorseven")
       .addEventListener("targetFound", () => {
-        this.markers.reference = true;
+        this.markers.studyrooms = true;
         document.querySelector("#goalseven").innerHTML = "Study Rooms ✔";
         this.check(this.markers);
       });
   },
   check: function (markers) {
     if (
-      Object.is(JSON.stringify(markers), JSON.stringify({ reference: true })) ==
-      true
+       Object.is(JSON.stringify(markers), JSON.stringify({ reference: true, computers: true, referencebooks: true, studyrooms: true, leisure: true, photocopier: true, elevator: true, circulation: true })) == true
     ) {
       document.getElementById("starter").style.display = "none";
       document.getElementById("ender").style.display = "block";
